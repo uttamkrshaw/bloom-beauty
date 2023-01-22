40282bd9027bebf7fad06112981a04450da7c39b
uttamkrshaw <uttamkr5599@gmail.com>
2023-01-22 23:56:49 +0530

    phase1

---
+++ src/Components/Pages/HomePage/Part2.jsx
@@ -0,0 +1,22 @@
+import { Image, Box, Flex } from "@chakra-ui/react";
+function Part2() {
+    return (
+        <>
+            <Flex w="full"
+                bg="#EEEEEE"
+                _dark={{
+                    bg: "#EEEEEE",
+                }}
+                alignItems="center"
+                justifyContent="center">
+                <Box margin="auto">
+                    <Image src='https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674099518_mamaearth_web_thick-strip.gif' alt='Dan Abramov' />
+                    <Image src="https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1673939041_unmissable-deals_web_10.gif" alt="" />
+                </Box>
+            </Flex>
+        </>
+    )
+}
+export default Part2
+// boxSize='6xl'
+
---
+++ src/Components/Pages/HomePage/Part7.jsx
@@ -0,0 +1,93 @@
+import { Flex, Box, Text, Image } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+function Part7() {
+    const slides = [
+        {
+            img: "https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674044098_paisa-vasool-elite-republic-l-1298x418.gif",
+        },
+        {
+            img: "https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674044096_skin-care-web.gif",
+        },
+        {
+            img: "https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674044100_makeup-web.gif",
+        },
+        {
+            img: "https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674044105_hair-care-web.gif",
+        },
+        {
+            img: "https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674044102_korean-web.gif",
+        }
+    ];
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slidesCount = slides.length;
+    const carouselStyle = {
+        transition: "all .5s",
+        ml: `-${currentSlide * 100}%`,
+    };
+    const SLIDES_INTERVAL_TIME = 3000;
+    const ANIMATION_DIRECTION = "right";
+    useEffect(() => {
+        const prevSlide = () => {
+            setCurrentSlide((s) => (s === 0 ? slidesCount - 1 : s - 1));
+        };
+
+        const nextSlide = () => {
+            setCurrentSlide((s) => (s === slidesCount - 1 ? 0 : s + 1));
+        };
+
+        const automatedSlide = setInterval(() => {
+            ANIMATION_DIRECTION.toLowerCase() === "left" ? prevSlide() : nextSlide();
+        }, SLIDES_INTERVAL_TIME);
+        return () => clearInterval(automatedSlide);
+    }, [slidesCount]);
+    return (
+        <Flex
+            w="full"
+            bg="#3e3e3e"
+            _dark={{
+                bg: "#EEEEEE",
+            }}
+            p={10}
+            alignItems="center"
+            justifyContent="center"
+        >
+            <Flex w="6xl" overflow="hidden">
+                <Flex pos="relative" h="400px" w="6xl" {...carouselStyle}>
+                    {slides.map((slide, sid) => (
+                        <Box key={`slide-${sid}`} flex="none" boxSize="full" shadow="md">
+                            <Text
+                                color="white"
+                                fontSize="xs"
+                                p="8px 12px"
+                                pos="absolute"
+                                top="0"
+                                whiteSpace="nowrap"
+                            >
+                                {sid + 1} / {slidesCount}
+                            </Text>
+                            <Image
+                                src={slide.img}
+                                alt="carousel image"
+                                boxSize="full"
+                                backgroundSize="cover"
+                            />
+                        </Box>
+                    ))}
+                </Flex>
+            </Flex>
+        </Flex>
+    );
+
+};
+
+export default Part7
+
+{/*
+ Changes Made : - 
+ line - 74 & 75  w="full" 
+ line - 68 bg- 3e3e3e for dark theme
+ line - 66 bg- edf3f8 for white theme 
+
+*/}
+
